b130a16d565b5aff53d1100539d26e8081464317
MadPumpkin3 <fpdjxpa37@gmail.com>
2023-08-08 23:41:40 +0900

    2023.8.8. study

--- config/views.py
+++ config/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 
 def index(request):
-    return render(request, "index.html")
+    if request.user.is_authenticated:
+        return redirect("/posts/feeds/")
+    else:
+        return redirect("/users/login/")
--- users/views.py
+++ users/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from users.forms import LoginForm
 
 
 # Create your views here.
 def login_view(request):
-    return render(request, "users/login.html")
+    if request.user.is_authenticated:
+        return redirect("/posts/feeds/")
+
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            user = authenticate(username=username, password=password)
+
+            if user:
+                login(request, user)
+                return redirect("/posts/feeds/")
+            else:
+                print("로그인에 실패했습니다")
+
+        context = {"form": form}
+        return render(request, "users/login.html", context)
+    else:
+        form = LoginForm()
+        context = {"form": form}
+        return render(request, "users/login.html", context)
