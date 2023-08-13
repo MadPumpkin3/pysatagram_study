a033a5533f31a1448c850cddf6b4c36216566f81
MadPumpkin3 <fpdjxpa37@gmail.com>
2023-08-13 19:52:30 +0900

    2023.8.13. study

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from users.forms import LoginForm
+from users.forms import LoginForm, SignupForm
+from users.models import User
 
 
 # Create your views here.
@@ -20,7 +21,7 @@ def login_view(request):
                 login(request, user)
                 return redirect("/posts/feeds/")
             else:
-                print("로그인에 실패했습니다")
+                form.add_error(None, "입력한 자격증명에 해당하는 사용자가 없습니다")
 
         context = {"form": form}
         return render(request, "users/login.html", context)
@@ -28,3 +29,44 @@ def login_view(request):
         form = LoginForm()
         context = {"form": form}
         return render(request, "users/login.html", context)
+
+
+def logout_view(request):
+    logout(request)
+
+    return redirect("/users/login/")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password1 = form.cleaned_data["password1"]
+            password2 = form.cleaned_data["password2"]
+            profile_image = form.cleaned_data["profile_image"]
+            short_description = form.cleaned_data["short_description"]
+
+            if password1 != password2:
+                form.add_error("password2", "비밀번호와 비밀번호 확인란의 값이 다릅니다")
+
+            if User.objects.filter(username=username).exists():
+                form.add_error("username", "입력한 사용자명은 이미 사용중입니다")
+
+            if form.errors:
+                context = {"form": form}
+                return render(request, "users/signup.html", context)
+
+            else:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password1,
+                    profile_image=profile_image,
+                    short_description=short_description,
+                )
+                login(request, user)
+                return redirect("/posts/feeds/")
+    else:
+        form = SignupForm()
+        context = {"form": form}
+        return render(request, "users/signup.html", context)
